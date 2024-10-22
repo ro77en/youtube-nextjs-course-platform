7bc0fc4f2f3611b477b662b201a31f1408215600
ro77en <lucas.mdantas@icloud.com>
2024-10-21 21:33:27 -0300

    feat: add tailwind css extended theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,16 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: "var(--text-color)",
+        error: "var(--error-color)",
+        paper: "var(--paper-color)",
+        primary: "var(--primary-color)",
+        background: "var(--background-color)",
+        "primary-contrast": "var(--primary-contrast-color)",
+      },
+    },
   },
   plugins: [],
 };
